d796ae95270cbab812b7a64d111dffaf9b35ef80
suyongfu <821314629@qq.com>
2023-03-09 10:09:50 +0800

    init Magnifier

---
+++ Assets/Magnifier/Magnifier.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class Magnifier : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        
+    }
+}

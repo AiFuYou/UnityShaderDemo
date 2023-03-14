5bf90e2b79b889976f9d3a34be53e1d4d7502677
suyongfu <821314629@qq.com>
2023-03-14 09:34:15 +0800

    FrostedGlass better

--- Assets/FrostedGlass/FrostedGlassImage.cs
+++ Assets/FrostedGlass/FrostedGlassImage.cs
@@ -5,9 +5,9 @@ public class FrostedGlassImage : Image
 {
     [SerializeField] [Range(1, 10)] private int _iterations = 3;
     [SerializeField] [Range(1, 10)] private int _downSample = 2;
+    private Material _blurMat;
 
     private static readonly int BlurTex = Shader.PropertyToID("_BlurTex");
-    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
     public int downSample
     {
@@ -32,8 +32,10 @@ public class FrostedGlassImage : Image
     protected override void OnEnable()
     {
         base.OnEnable();
-        material = new Material(Shader.Find("Demo/FrostedGlass"));
-        material.hideFlags = HideFlags.HideAndDontSave;
+        material = new Material(Shader.Find("Demo/FrostedGlass"))
+        {
+            hideFlags = HideFlags.HideAndDontSave
+        };
         GaussianBlur();
     }
 
@@ -46,23 +48,24 @@ public class FrostedGlassImage : Image
         var w = oriT.width / downSample;
         var h = oriT.height / downSample;
 
-        var mat = new Material(Shader.Find("Demo/GaussianBlur"));
+        _blurMat = _blurMat != null ? _blurMat : new Material(Shader.Find("Demo/GaussianBlur"));
+
         var buffer0 = RenderTexture.GetTemporary(w, h);
         Graphics.Blit(oriT, buffer0);
         for (var i = 0; i < iterations; i++)
         {
             var buffer1 = RenderTexture.GetTemporary(w, h);
             buffer1.filterMode = FilterMode.Bilinear;
-            Graphics.Blit(buffer0, buffer1, mat, 0);
-        
+            Graphics.Blit(buffer0, buffer1, _blurMat, 0);
+
             // 释放buffer0并将buffer1赋值给buffer0
             RenderTexture.ReleaseTemporary(buffer0);
             buffer0 = buffer1;
-        
+
             buffer1 = RenderTexture.GetTemporary(w, h);
             buffer1.filterMode = FilterMode.Bilinear;
-            Graphics.Blit(buffer0, buffer1, mat, 1);
-        
+            Graphics.Blit(buffer0, buffer1, _blurMat, 1);
+
             // 释放buffer0并将buffer1赋值给buffer0
             RenderTexture.ReleaseTemporary(buffer0);
             buffer0 = buffer1;
